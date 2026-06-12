71372c1eaabec0bf714e84776d9c5eb708fd7322
Roman <yamshchikovr11@gmail.com>
2026-06-12 20:16:27 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { useState } from 'react';
 
 export const App = () => {
+  const [movie, setMovie] = useState(moviesFromServer);
+  const handleAdd = newMovie => setMovie([...movie, newMovie]);
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movie} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={handleAdd} />
       </div>
     </div>
   );
